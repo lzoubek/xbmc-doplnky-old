3d0a89b91740a44fe570c9ae90f8c15eeaefdb89
Libor Zoubek <lzoubek@jezzovo.net>
2011-09-13 00:32:25 +0200

    added build script + all addons released using it

---
+++ release-all.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# taken grom xbmc-czech-sf.net
+
+TOOLS=$(dirname "$0")
+
+BUILD_DIR=repo
+
+for addonFile in $(ls -l | grep "^d" | gawk -F' ' '{print $9}') ; do
+    dirname=$addonFile
+    if [ ! -f $addonFile/addon.xml ] ; then
+	echo "$addonFile/addon.xml does not exist, skipping"
+	continue
+    fi
+    addon_id=$("$TOOLS/get_addon_attribute" "$addonFile/addon.xml" "id")
+    addon_version=$("$TOOLS/get_addon_attribute" "$addonFile/addon.xml" "version")
+
+    if [ -z "$addon_id" ] ; then
+        echo "Addon id not found!" >&2
+        exit 1
+    fi
+
+    if [ -z "$addon_version" ] ; then
+        echo "Addon id not found!" >&2
+        exit 2
+    fi
+
+    target_dir="$BUILD_DIR/$addon_id"
+    if [ ! -d "$target_dir" ] ; then
+        mkdir "$target_dir"
+    fi
+
+    echo "Packing $addon_id $addon_version"
+
+    # make package
+    package="$target_dir/$addon_id-$addon_version.zip"
+    if [ -e "$package" ] ; then
+        rm "$package"
+    fi
+    zip -FS -r "$package" "$dirname" -x "*.py[oc]" ".*"
+
+    # copy changelog file
+    changelog=$(ls "$dirname"/[Cc]hangelog.txt)
+    if [ -f "$changelog" ] ; then
+        cp "$changelog" "$target_dir"/changelog-$addon_version.txt
+    fi
+
+    # copy icon file
+    icon="$dirname"/icon.png
+    if [ -f "$icon" ] ; then
+        cp "$icon" "$target_dir"/
+    fi
+    echo
+done
+
