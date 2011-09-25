2dbb1e12f55f359f0b149cb031a7e12f6bc38272
Libor Zoubek <lzoubek@jezzovo.net>
2011-09-26 00:48:38 +0200

    serialycz 1.0.3

--- plugin.video.serialycz.cz/default.py
+++ plugin.video.serialycz.cz/default.py
@@ -19,121 +19,15 @@
 # *  http://www.gnu.org/copyleft/gpl.html
 # *
 # */
-import urllib2,re,os,md5
-import xbmcaddon,xbmc,xbmcgui,xbmcplugin
-import util,resolver
+import re,os
+import xbmcaddon,util
 
 __scriptid__   = 'plugin.video.serialycz.cz'
 __scriptname__ = 'serialycz.cz'
 __addon__      = xbmcaddon.Addon(id=__scriptid__)
 __language__   = __addon__.getLocalizedString
 
-BASE_URL='http://www.serialycz.cz/'
+sys.path.append( os.path.join ( __addon__.getAddonInfo('path'), 'resources','lib') )
 
-def _get_meta(name,link):
-	# load meta from disk or download it (slow for each serie, thatwhy we cache it)
-	local = xbmc.translatePath(__addon__.getAddonInfo('profile'))
-	if not os.path.exists(local):
-		os.makedirs(local)
-	m = md5.new()
-	m.update(name)
-	image = os.path.join(local,m.hexdigest()+'_img.png')
-	plot = os.path.join(local,m.hexdigest()+'_plot.txt')
-	if not os.path.exists(image):
-		data = util.request(link)
-		data = util.substr(data,'<div id=\"archive-posts\"','</div>')
-		m = re.search('<a(.+?)href=\"(?P<url>[^\"]+)', data, re.IGNORECASE | re.DOTALL)
-		if not m == None:
-			data = util.request(m.group('url'))
-			_get_image(data,image)
-			_get_plot(data,plot)
-	return image,_load(plot)
-	
-
-def _save(data,local):
-	print 'Saving file %s' % local
-	f = open(local,'w')
-	f.write(data)
-	f.close()
-
-def _load(file):
-	if not os.path.exists(file):
-		return ''
-	f = open(file,'r')
-	data = f.read()
-	f.close()
-	return data
-
-def _get_plot(data,local):
- 		data = util.substr(data,'<div class=\"entry-content\"','</p>')
-		m = re.search('<(strong|b)>(?P<plot>(.+?))<', data, re.IGNORECASE | re.DOTALL)
-		if not m == None:
-			_save(util.decode_html(m.group('plot')).encode('utf-8'),local)
-def _get_image(data,local):
- 		data = util.substr(data,'<div class=\"entry-photo\"','</div>')
-		m = re.search('<img(.+?)src=\"(?P<img>[^\"]+)', data, re.IGNORECASE | re.DOTALL)
-		if not m == None:
-			print ' Downloading %s' % m.group('img')
-			_save(util.request(m.group('img')),local)
-	
-def list_series():
-	data = util.substr(util.request(BASE_URL),'<div id=\"primary\"','</div>')
-	pattern='<a href=\"(?P<link>[^\"]+)[^>]+>(?P<name>[^<]+)</a>'	
-	util.add_dir('[B]Nejnovější epizody[/B]',{'newest':'list'},'')
-	for m in re.finditer(pattern, util.substr(data,'Seriály</a>','</ul>'), re.IGNORECASE | re.DOTALL):
-		image,plot = _get_meta(m.group('name'),m.group('link'))
-		util.add_dir(m.group('name'),{'serie':m.group('link')[len(BASE_URL):]},image,infoLabels={'Plot':plot})
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-def list_episodes(url):	
-	data = util.request(BASE_URL+url)
-	data = util.substr(data,'<div id=\"archive-posts\"','</div>')
-	m = re.search('<a(.+?)href=\"(?P<url>[^\"]+)', data, re.IGNORECASE | re.DOTALL)
-	if not m == None:
-		data = util.request(m.group('url'))
-		for m in re.finditer('<a href=\"(?P<link>[^\"]+)(.+?)(<strong>|<b>)(?P<name>[^<]+)', util.substr(data,'<div class=\"entry-content','</div>'), re.IGNORECASE | re.DOTALL):
-			add_video(m.group('name'),m.group('link')[len(BASE_URL):])
-		xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-def newest_episodes():
-	data = util.substr(util.request(BASE_URL+'category/new-episode/'),'<div id=\"archive-posts\"','</ul>')
-	pattern='<img(.+?)src=\"(?P<img>[^\"]+)(.+?)<a href=\"(?P<link>[^\"]+)[^>]+>(?P<name>[^<]+)</a>'	
-	for m in re.finditer(pattern, data, re.IGNORECASE | re.DOTALL):
-		add_video(m.group('name'),m.group('link')[len(BASE_URL):],m.group('img'))	
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-def add_video(name,url,image=''):
-	return util.add_video(name,{'play':url},image)
-
-def play(url):
-	streams = None
-	data = util.substr(util.request(BASE_URL+url),'<div id=\"content\"','<div id=\"sidebar')
-	resolved = []
-	for m  in re.finditer('<iframe(.+?)src=[\'\"](?P<url>(.+?))[\'\"]',data,re.IGNORECASE | re.DOTALL ):
-		streams = resolver.resolve(m.group('url'))
-		if not streams == None:
-			resolved.extend(streams)
-	if streams == []:
-		xbmcgui.Dialog().ok(__scriptname__,'Video neni dostupne, zkontrolujte,[CR]zda funguje na webu')
-		return
-	if not resolved == []:
-		stream = resolved[0]
-		if len(resolved) > 1:
-			dialog = xbmcgui.Dialog()
-			ret = dialog.select('Zvolte zdroj', resolved)
-			if ret > 0:
-				stream = resolved[ret]
-		print 'Sending %s to player' % stream
-		li = xbmcgui.ListItem(path=stream,iconImage='DefaulVideo.png')
-		return xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, li)
-	xbmcgui.Dialog().ok(__scriptname__,'Prehravani vybraneho videa z tohoto zdroje[CR]zatim neni podporovano.')
-
-p = util.params()
-if p=={}:
-	list_series()
-if 'newest' in p.keys():
-	newest_episodes()
-if 'serie' in p.keys():
-	list_episodes(p['serie'])
-if 'play' in p.keys():
-	play(p['play'])
+import serialy
+serialy.handle(util.params())
