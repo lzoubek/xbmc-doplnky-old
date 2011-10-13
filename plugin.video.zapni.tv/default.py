e514c5fec6f284f5dd257919f862fbed32e2f693
Libor Zoubek <lzoubek@jezzovo.net>
2011-10-13 17:05:15 +0200

    zapni.tv 1.0.1

--- plugin.video.zapni.tv/default.py
+++ plugin.video.zapni.tv/default.py
@@ -22,18 +22,19 @@
 
 import urllib2,re,os,random
 import xbmcaddon,xbmc,xbmcgui,xbmcplugin
+import util,resolver
 
 __scriptid__   = 'plugin.video.zapni.tv'
 __scriptname__ = 'zapni.tv'
 __addon__      = xbmcaddon.Addon(id=__scriptid__)
 __language__   = __addon__.getLocalizedString
 
-BASE_URL='http://zapnitv.biz/'
+BASE_URL='http://zapni.tv/'
 
 def parse_asx(url):
 	if not url.endswith('asx'):
 		return url
-	data = request(url)
+	data = util.request(url)
 	refs = re.compile('.*<Ref href = \"([^\"]+).*').findall(data,re.IGNORECASE|re.DOTALL|re.MULTILINE)
 	urls = []
 	for ref in refs:
@@ -45,112 +46,73 @@ def parse_asx(url):
 		return ''
 	return urls[-1]
 
-def request(url):
-	req = urllib2.Request(url)
-	response = urllib2.urlopen(req)
-	data = response.read()
-	response.close()
-	return data
-
-def add_dir(name,id,logo):
-	logo = replace(logo)
-	name = replace(name,None,'No name').replace('&amp;','&')
-        u=sys.argv[0]+"?"+id
-	liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png",thumbnailImage=logo)
-        liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
-
-def replace(obj,what=None,replacement=''):
-	if obj == what:
-		return replacement
-	return obj
-
-def get_substr(data,start,end):
-	i1 = data.find(start)
-	i2 = data.find(end,i1)
-	return data[i1:i2]
-
-
-def get_params():
-        param={}
-        paramstring=sys.argv[2]
-        if len(paramstring)>=2:
-                params=sys.argv[2]
-                cleanedparams=params.replace('?','')
-                if (params[len(params)-1]=='/'):
-                        params=params[0:len(params)-2]
-                pairsofparams=cleanedparams.split('&')
-                param={}
-                for i in range(len(pairsofparams)):
-                        splitparams={}
-                        splitparams=pairsofparams[i].split('=')
-                        if (len(splitparams))==2:
-                                param[splitparams[0]]=splitparams[1]
-	return param
-
 def add_stream(name,url,bitrate,logo):
 	bit = 0
 	try:
 		bit = int(bitrate)
 	except:
 		pass
-	name = replace(name,None,'No name').replace('&amp;','&')
 	if bit > 0:
 		name = "%s | %s kbps" % (name,bit)
-	logo = replace(logo)
-	url=sys.argv[0]+"?play="+url
-	li=xbmcgui.ListItem(name,path = url,iconImage="DefaultVideo.png",thumbnailImage=logo)
-        li.setInfo( type="Video", infoLabels={ "Title": name} )
-	li.setProperty("IsPlayable","true")
-        return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=li,isFolder=False)
+	return util.add_video(name,{'play':url},logo)
 
 def list_categories():
-	data = request(BASE_URL)
+	data = util.request(BASE_URL)
 	pattern='<li><a onmousedown=\'[^\']+\'( class=\'divider\')? href=\'kat\.php\?id=(?P<link>[^\']+)[^>]+>(?P<cat>[^<]+)</a>'	
-	for m in re.finditer(pattern, get_substr(data,'<li class=\'menu\'><p>Kategorie</p>','</ul>'), re.IGNORECASE | re.DOTALL):
+	for m in re.finditer(pattern, util.substr(data,'<li class=\'menu\'><p>Kategorie</p>','</ul>'), re.IGNORECASE | re.DOTALL):
 		if m.group('link').find('porno') > 0:
 			continue
-		add_dir(m.group('cat'),'cat='+m.group('link'),'')
-	add_dir('Ostatní','cat=','')
+		util.add_dir(m.group('cat'),{'cat':m.group('link')})
+	util.add_dir('Ostatní',{'cat':''})
 	xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 def list_category(id):	
-	data = request(BASE_URL+'kat.php?id='+id)
-	data = get_substr(data,'class=\'centerContent\'','<p class=\'strankovani\'>')
+	data = util.request(BASE_URL+'kat.php?id='+id)
+	data = util.substr(data,'class=\'centerContent\'','<p class=\'strankovani\'>')
 	pattern='<a href=\"play.php\?id=(?P<id>[^\"]+)[^>] title=\"(?P<name>[^\"]+)\"><img align=\"left\" style=\"[^\']+\'(?P<img>[^\']+)[^>]+></a>[^\:]+\:[ ]?<b>(?P<bitrate>[\d]+)'
 	for m in re.finditer(pattern, data, re.IGNORECASE | re.DOTALL):
 		add_stream(m.group('name'),m.group('id'),m.group('bitrate'),BASE_URL+m.group('img'))
 	xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 def play(id):
-	for gen in resolve_stream(id):
-		for url in gen:
-			print 'Sending %s to player' % url
-			li = xbmcgui.ListItem(path=url,iconImage='DefaulVideo.png')
-			return xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, li)
+	url = resolve_stream(id)
+	if not url == None:
+		print 'Sending %s to player' % url
+		li = xbmcgui.ListItem(path=url,iconImage='DefaulVideo.png')
+		return xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, li)
 	xbmcgui.Dialog().ok(__scriptname__,__language__(30000))
 
 def resolve_stream(id):
-	data = request(BASE_URL+'play.php?id='+id)
-	data = get_substr(data,'class=\'centerContent\'','class=\'side-border-right\'')
+	data = util.request(BASE_URL+'play.php?id='+id)
+	data = util.substr(data,'class=\'centerContent\'','class=\'side-border-right\'')
 	for f in [_wmp,_rp,_vlc,_flash_zakladni,_flash_pls,_seeon,_borec,_apache_cz]:
-		yield f(data)
+		url = f(data)
+		if not url == None:
+			return url
 
 def _rp(data):
 	m = re.search('<embed type=\"audio/x-pn-realaudio-plugin\" src=\"(?P<url>stream/real.php[^\"]+)', data, re.IGNORECASE | re.DOTALL)
 	if not m == None:
-		yield BASE_URL+m.group('url')
+		return BASE_URL+m.group('url')
 
 def _vlc(data):
-	m = re.search('<br><a href=\"(?P<url>stream/vlc.php[^\"]+)', data, re.IGNORECASE | re.DOTALL)
+	m = re.search('<br><a href=\"(?P<url>stream/vlc[^\"]+)', data, re.IGNORECASE | re.DOTALL)
 	if not m == None:
-		yield BASE_URL+m.group('url')
+		playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+		playlist.clear()
+		url = BASE_URL+m.group('url')
+		li = xbmcgui.ListItem(label='Zacatek',path='http://www.zapni.tv/stream/videa/zacatek.wmv')
+		playlist.add('http://www.zapni.tv/stream/videa/zacatek.wmv',li,0)
+		for i in range(0,3):
+			li = xbmcgui.ListItem(label='Stream',path=url)
+			playlist.add(url,li,0)
+		return url
 
 def _flash_pls(data):
-	m = re.search('flashvars=\'playlistfile=(?P<url>[^\\&]+)',get_substr(data,'<embed','/>'), re.IGNORECASE | re.DOTALL)
+	m = re.search('flashvars=\'playlistfile=(?P<url>[^\\&]+)',util.substr(data,'<embed','/>'), re.IGNORECASE | re.DOTALL)
 	if not m == None:
 		# there is a file in RSS playlist format, we need to get it, parse and pass it to video playlist
-		pls = request(BASE_URL+m.group('url'))
+		pls = util.request(BASE_URL+m.group('url'))
 		playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
 		playlist.clear()
 		count = 0
@@ -162,38 +124,38 @@ def _flash_pls(data):
 			if count > 1:
 				url = m.group('url')
 			count+=1
-		yield url
+		return url
 
 def _flash_zakladni(data):
 	m = re.search('<embed src=\"(\\./)?stream/zakladni.swf\"(.+?)flashvars=\"file=(?P<url>[^\\&]+)',data, re.IGNORECASE | re.DOTALL)
 	if not m == None:
-		yield m.group('url')
+		return m.group('url')
 
 def _seeon(data):
 	m = re.search('<iframe src=\'http\://www.seeon.tv(.+?)channel=(?P<id>[\d]+)', data, re.IGNORECASE | re.DOTALL)
 	if not m == None:
 		url = 'http://www.seeon.tv/view/'+m.group('id')
-		html = request(url)
+		html = util.request(url)
 		swf_url, play = re.search('data="(.+?)".+?file=(.+?)\.flv', html, re.DOTALL).group(1, 2)
 		rtmp = 'rtmp://live%d.seeon.tv/edge' % (random.randint(1, 10)) 
 		rtmp += '/%s swfUrl=%s pageUrl=%s tcUrl=%s' % (play, swf_url, url, rtmp)
-		yield rtmp
+		return rtmp
 def _apache_cz(data):
 	# not yet working
 	m = re.search('src=\"http\://www\.server-apache\.cz(.+?)(?P<url>rtmp\://[^\"]+)', data, re.IGNORECASE | re.DOTALL)
 	if not m == None:
-		yield m.group('url')
+		return m.group('url')
 def _borec(data):
 	m = re.search('open\(\"http://televize.borec.cz/\?id=(?P<id>[^\"]+)\"', data, re.IGNORECASE | re.DOTALL)
 	if not m == None:
-		yield 'http://cdn.livestream.com/grid/LSPlayer.swf?channel=%s&autoPlay=true'%m.group('id')
+		return 'http://cdn.livestream.com/grid/LSPlayer.swf?channel=%s&autoPlay=true'%m.group('id')
 
 def _wmp(data):
 	m = re.search('<param name=\"url\" value=\"(?P<url>stream/wmp\.php[^\"]+)\">', data, re.IGNORECASE | re.DOTALL)
 	if not m == None:
-		yield parse_asx(BASE_URL+m.group('url'))
+		return parse_asx(BASE_URL+m.group('url'))
 	
-params=get_params()
+params=util.params()
 if params=={}:
 	list_categories()
 if 'cat' in params.keys():
