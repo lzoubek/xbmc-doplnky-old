9852098fabdfbb640bb82ace5195e23850bf4cd7
Libor Zoubek <lzoubek@jezzovo.net>
2011-10-26 23:18:52 +0200

    initial version of script.usage.tracker

---
+++ script.usage.tracker/resources/lib/gui.py
@@ -0,0 +1,103 @@
+#/*
+# *      Copyright (C) 2010 lzoubek
+# *
+# *
+# *  This Program is free software; you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2, or (at your option)
+# *  any later version.
+# *
+# *  This Program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with this program; see the file COPYING.  If not, write to
+# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# *  http://www.gnu.org/copyleft/gpl.html
+# *
+# */
+import sys,os,time,re,traceback
+import xbmc,xbmcaddon,xbmcgui,xbmcplugin
+
+import tracker
+
+ADDONS = 1001
+
+class MainWindow ( xbmcgui.WindowXMLDialog ) :
+
+	def __init__( self, *args, **kwargs ):
+		super(xbmcgui.WindowXMLDialog, self).__init__(args,kwargs)
+		self.trSettings = tracker.TrackerSettings(__addon__)
+
+	def onFocus (self,controlId ):
+		self.controlId=controlId
+
+	def onInit (self):
+		self._refresh_addons()
+		self._refresh_addon_details()
+
+	def _refresh_addons(self):
+		list = self.getControl(ADDONS)
+		list.reset()
+		for add in self.trSettings.getSubscribers():
+			add_inst = xbmcaddon.Addon(id=add)
+			li = xbmcgui.ListItem(label=add_inst.getAddonInfo('name'),iconImage=add_inst.getAddonInfo('icon'))
+			li.setProperty('id',add)
+			enabled = self.trSettings.canDoReport(add)
+			if enabled:
+				enStr = __addon__.getLocalizedString(30004)
+			else:
+				enStr = __addon__.getLocalizedString(30005)
+			print enStr
+			li.setProperty('enabled',enStr)
+			if enabled:
+				li.setProperty('isenabled','True')
+			termsID = self.trSettings.getTermsStringID(add)
+			if termsID < 0:
+				# use defaults
+				terms = __addon__.getLocalizedString(30000) % (add_inst.getAddonInfo('name'),'')
+			else:
+				terms = __addon__.getLocalizedString(30000)% (add_inst.getAddonInfo('name'),add_inst.getLocalizedString(termsID))
+			li.setProperty('terms',terms)
+			list.addItem(li)
+
+	def _refresh_addon_details(self):
+		print 'refresh'
+		selected = self.getControl(ADDONS).getSelectedItem()
+
+	def onAction(self, action):
+		#print 'Action id=%s buttonCode=%s amount1=%s amount2=%s' % (action.getId(),action.getButtonCode(),action.getAmount1(),action.getAmount2())
+		if action.getId() in [9,10]:
+			self.close()
+		if action.getId() in [3,4] and self.getFocusId() == ADDONS:
+			self._refresh_addon_details()
+			
+#	if str(action.getId()) in ACTIONS:
+#			command = ACTIONS[str(action.getId())]
+#			print 'action: '+command
+#			self._exec_command(command)
+
+	def onClick( self, controlId ):
+		print controlId
+		if controlId in [1003, 1001]:
+			selected_index = self.getControl(ADDONS).getSelectedPosition()
+			addon = self.getControl(ADDONS).getSelectedItem().getProperty('id')
+			add_inst = xbmcaddon.Addon(addon)
+			isenabled = self.getControl(ADDONS).getSelectedItem().getProperty('isenabled') == 'True'
+			if isenabled:
+				ret = xbmcgui.Dialog().yesno(__addon__.getAddonInfo('name'),(__addon__.getLocalizedString(30007) % add_inst.getAddonInfo('name')))
+			else:
+				ret = xbmcgui.Dialog().yesno(__addon__.getAddonInfo('name'),(__addon__.getLocalizedString(30006) % add_inst.getAddonInfo('name')))
+			
+			confirmed = ret == 1
+			if confirmed:
+				self.trSettings.setCanReport(addon,not isenabled)
+				self.trSettings.save()
+				self._refresh_addons()
+				self.getControl(ADDONS).selectItem(selected_index)
+#		if str(controlId) in CLICK_ACTIONS:
+#			command = CLICK_ACTIONS[str(controlId)]
+#			print 'click action: '+command
+#			self._exec_command(command)
---
+++ script.usage.tracker/resources/lib/tracker.py
@@ -0,0 +1,200 @@
+# -*- coding: UTF-8 -*-
+#/*
+# *      Copyright (C) 2011 Libor Zoubek
+# *
+# *
+# *  This Program is free software; you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2, or (at your option)
+# *  any later version.
+# *
+# *  This Program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with this program; see the file COPYING.  If not, write to
+# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# *  http://www.gnu.org/copyleft/gpl.html
+# *
+# */
+import simplejson as json
+import xbmc,os,random,sys
+
+import google
+class TrackerSettings(object):
+
+	def __init__(self,addon):
+		self.services = [google]
+		local = xbmc.translatePath(addon.getAddonInfo('profile'))
+		if not os.path.exists(local):
+			os.makedirs(local)
+		self.settingFile = os.path.join(local,'tracking.json')
+		
+		if os.path.exists(self.settingFile):
+			f = open(self.settingFile,'r')
+			self.data = json.loads(unicode(f.read().decode('utf-8','ignore')))
+			f.close()
+		else:
+			self.data = {}
+			self.data['id'] = {}
+			for serv in self.services:
+				self.data['id'][serv.__name__] = serv.createInstanceID()
+		#	self.data['addons'] = {'script.usage.tracker':{'enabled':True,'terms':-1},'plugin.video.kinotip2.cz':{'enabled':True,'terms':-1}}
+			self.data['addons'] = {}
+			self.save()
+			
+	def save(self):
+		f = open(self.settingFile,'w')
+		f.write(json.dumps(self.data,ensure_ascii=True))
+		f.close()
+
+	def getInstanceID(self,service):
+		return self.data['id'][service]
+
+	def canDoReport(self,addon):
+		subscribers = self.getSubscribers()
+		if addon in subscribers:
+			return subscribers[addon]['enabled']
+	def setCanReport(self,addon,canReport):
+		subscribers = self.getSubscribers()
+		if addon in subscribers:
+			subscribers[addon]['enabled'] = canReport
+
+	def getTermsStringID(self,addon):
+		subscribers = self.getSubscribers()
+		if addon in subscribers:
+			return subscribers[addon]['terms']
+	
+	def getSubscribers(self):
+		return self.data['addons']
+
+	def addAddon(self,addon,subscribed,conditions):
+		self.data['addons'][addon] = {}
+		self.data['addons'][addon]['enabled'] = subscribed
+		self.data['addons'][addon]['terms'] = conditions
+		self.save()
+
+class TrackerInfo(object):
+	def __init__(self):
+		pass
+
+	def _getUserAgent(self,version,language):
+		platform = sys.platform
+		sp = version.find(' ')
+		if sp > 0:
+			version = version[:sp]
+		if platform.startswith('linux'):
+			return 'XBMC/%s (X11; U; Linux i686; %s)' % (version,language)
+		if platform.startswith('Win'):
+			return 'XBMC/%s (Windows; U; Windows NT; %s)' % (version,language)
+		if platform.startswith('darwin'):
+			return 'XBMC/%s (Macintosh; U; Intel Mac OS X; %s)' % (version,language)
+		else:
+			print '[script.usage.tracker] Unknown platform %s, please report a bug, plugin needs to be fixed' % sys.platform
+			return 'XBMC/%s (X11; U; Linux i686; %s)' % (version,language)
+
+
+	def getSystemInfo(self):
+		# some stupid defaults
+		info = {'resolution':'640x480','language':'en','useragent':self._getUserAgent('Unknown','en')}
+		
+		# get info via JSON RPC, try to be compatible with both dharma & eden APIs
+		try:
+			data = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "System.GetInfoLabels", "id":1,"params": ["System.BuildVersion","System.ScreenHeight","System.ScreenWidth","System.KenrelVersion","System.AddonTitle(script.mpdc)"]}')
+		except:
+			pass
+		try:	
+			data = xbmc.executeJSONRPC('{"jsonrpc" : "2.0", "method": "XBMC.GetInfoLabels", "id" :1, "params": {"labels" : ["System.BuildVersion","System.ScreenHeight","System.ScreenWidth","System.KenrelVersion","System.AddonTitle(script.mpdc)","System.Language"]}}')
+		except:
+			pass
+		# process results
+		try:
+			data = json.loads(data)
+			info['resolution'] = '%sx%s' %(data['result']['System.ScreenWidth'],data['result']['System.ScreenHeight'])
+			info['language'] = getLanguageCode(data['result']['System.Language'])
+			info['useragent'] = self._getUserAgent(data['result']['System.BuildVersion'],info['language'])
+			info['colordepth'] = '24-bit'
+		except:
+			pass
+		return info
+
+
+def getLanguageCode( id ):
+  languages = { 
+  	"None"                       : "none",
+    "Albanian"                   : "sq",
+    "Arabic"                     : "ar",
+    "Belarusian"                 : "hy",
+    "Bosnian"                    : "bs",
+    "BosnianLatin"               : "bs",
+    "Bulgarian"                  : "bg",
+    "Catalan"                    : "ca",
+    "Chinese"                    : "zh",
+    "Croatian"                   : "hr",
+    "Czech"                      : "cs",
+    "Danish"                     : "da",
+    "Dutch"                      : "nl",
+    "English"                    : "en",
+    "Esperanto"                  : "eo",
+    "Estonian"                   : "et",
+    "Farsi"                      : "fa",
+    "Persian"                    : "fa",
+    "Finnish"                    : "fi",
+    "French"                     : "fr",
+    "Galician"                   : "gl",
+    "Georgian"                   : "ka",
+    "German"                     : "de",
+    "Greek"                      : "el",
+    "Hebrew"                     : "he",
+    "Hindi"                      : "hi",
+    "Hungarian"                  : "hu",
+    "Icelandic"                  : "is",
+    "Indonesian"                 : "id",
+    "Italian"                    : "it",
+    "Japanese"                   : "ja",
+    "Kazakh"                     : "kk",
+    "Korean"                     : "ko",
+    "Latvian"                    : "lv",
+    "Lithuanian"                 : "lt",
+    "Luxembourgish"              : "lb",
+    "Macedonian"                 : "mk",
+    "Malay"                      : "ms",
+    "Norwegian"                  : "no",
+    "Occitan"                    : "oc",
+    "Polish"                     : "pl",
+    "Portuguese"                 : "pt",
+    "PortugueseBrazil"           : "pb",
+    "Portuguese (Brazil)"        : "pb",
+    "Brazilian"                  : "pb",
+    "Romanian"                   : "ro",
+    "Russian"                    : "ru",
+    "SerbianLatin"               : "sr",
+    "Serbian"                    : "sr",
+    "Slovak"                     : "sk",
+    "Slovenian"                  : "sl",
+    "Spanish"                    : "es",
+    "Swedish"                    : "sv",
+    "Syriac"                     : "syr",
+    "Thai"                       : "th",
+    "Turkish"                    : "tr",
+    "Ukrainian"                  : "uk",
+    "Urdu"                       : "ur",
+    "Vietnamese"                 : "vi",
+    "English (US)"               : "en",
+    "English (UK)"               : "en",
+    "Portuguese (Brazilian)"     : "pt-br",
+    "Español (Latinoamérica)"    : "es",
+    "Español (España)"           : "es",
+    "Spanish (Latin America)"    : "es",
+    "Español"                    : "es",
+    "Spanish (Spain)"            : "es",
+    "Chinese (Traditional)"      : "zh",
+    "Chinese (Simplified)"       : "zh",
+    "Portuguese-BR"              : "pb",
+    "All"                        : "all"
+  }
+  if id in languages:
+	return languages[ id ]
+  return 'none'
