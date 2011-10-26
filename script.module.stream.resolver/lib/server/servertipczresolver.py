b1075a923a9c69d5659bb8f103c57a0c61f55fb8
Libor Zoubek <lzoubek@jezzovo.net>
2011-10-26 21:48:41 +0200

    added servertip.cz resolver

---
+++ script.module.stream.resolver/lib/server/servertipczresolver.py
@@ -0,0 +1,40 @@
+# -*- coding: UTF-8 -*-
+#/*
+# *      Copyright (C) 2011 Libor Zoubek
+# *
+# *
+# *  This Program is free software; you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2, or (at your option)
+# *  any later version.
+# *
+# *  This Program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with this program; see the file COPYING.  If not, write to
+# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# *  http://www.gnu.org/copyleft/gpl.html
+# *
+# */
+import re,util,urllib
+
+def supports(url):
+	return not _regex(url) == None
+
+# returns the steam url
+def url(url):
+	if supports(url):
+		data = util.request(url)
+		data = util.substr(data,'<div id=\"player_code','</div')
+		try:
+			video_id = re.search('flv\|\|([^\|]+)',data).group(1)
+			return ['http://servertip.cz/cgi-bin/dl.cgi/%s/video.flv' % video_id]
+		except:
+			pass
+
+def _regex(url):
+	return re.search('servertip\.cz',url,re.IGNORECASE | re.DOTALL)
+
