4c5afff9ea5c012694a56f1ba7c37c3c35eeb30e
Libor Zoubek <lzoubek@jezzovo.net>
2012-01-06 18:00:41 +0100

    videozer resolver working again, thanks to Lynx187

--- script.module.stream.resolver/lib/server/videozerresolver.py
+++ script.module.stream.resolver/lib/server/videozerresolver.py
@@ -17,28 +17,139 @@
 # *  http://www.gnu.org/copyleft/gpl.html
 # *
 # */
+# thanks to Lynx187 and his fix in https://github.com/t0mm0/xbmc-urlresolver
 
 import re,util,base64
 
-
-__PREFER_HD__=True
+from base64 import b64decode
+from binascii import unhexlify
 
 def supports(url):
 	return not _regex(url) == None
+
+def _regex(url):
+	return  re.search('videozer.com/embed/(?P<id>[_\-\w\d]+)',url,re.IGNORECASE | re.DOTALL)
 
 def url(url):
 	m = _regex(url)
 	if not m == None:
 		stream = None
 		data = util.request('http://www.videozer.com/player_control/settings.php?v=%s&em=TRUE&fv=v1.1.12' % m.group('id'))
-		data = eval(data.replace('true','True').replace('false','False').replace('null','None'))		
-		if len(data['cfg']['quality']) > 0:
-			stream = base64.decodestring(data['cfg']['quality'][0]['u'])
-		for q in data['cfg']['quality']:
-			if q['l'] == 'HQ' and __PREFER_HD__:
-				return [base64.decodestring(q['u'])]
-		if not stream == None:
-			return [stream]
+		json = data.replace('false','False').replace('true','True').replace('null','None')
+		aData = eval('('+json+')')
+		max_res = 99999
+		r = re.finditer('"l".*?:.*?"(.+?)".+?"u".*?:.*?"(.+?)"', json)
+		chosen_res = 0
+		if r:
+			for match in r:
+				res, url = match.groups()
+				if (res == 'LQ' ): res = 240
+				elif (res == 'SD') : res = 480
+				else : res = 720
+				if res > chosen_res and res <= max_res:
+					stream_url_part1 = url.decode('base-64')
+      					chosen_res = res
+ 		else:
+			return
 
-def _regex(url):
-	return  re.search('videozer.com/embed/(?P<id>[_\-\w\d]+)',url,re.IGNORECASE | re.DOTALL)
+
+	# Decode the link from the json data settings.
+		spn_ik = unhexlify(__decrypt(aData["cfg"]["login"]["spen"], aData["cfg"]["login"]["salt"], 950569)).split(';')
+		spn = spn_ik[0].split('&')
+		ik = spn_ik[1]
+
+		for item in ik.split('&') :
+			temp = item.split('=')
+			if temp[0] == 'ik' : 
+				key = __get_key(temp[1])
+
+		sLink = ""
+		for item in spn :
+			item = item.split('=')
+			if(int(item[1])==1):
+				sLink = sLink + item[0]+ '=' + __decrypt(aData["cfg"]["info"]["sece2"], aData["cfg"]["environment"]["rkts"], key) + '&'  #decrypt32byte
+			elif(int(item[1]==2)):
+				sLink = sLink + item[0]+ '=' + __decrypt(aData["cfg"]["ads"]["g_ads"]["url"],aData["cfg"]["environment"]["rkts"], key) + '&'	
+			elif(int(item[1])==3):
+				sLink = sLink + item[0]+ '=' + __decrypt(aData["cfg"]["ads"]["g_ads"]["type"],aData["cfg"]["environment"]["rkts"], key,26,25431,56989,93,32589,784152) + '&'	
+			elif(int(item[1])==4):
+				sLink = sLink + item[0]+ '=' + __decrypt(aData["cfg"]["ads"]["g_ads"]["time"],aData["cfg"]["environment"]["rkts"], key,82,84669,48779,32,65598,115498) + '&'
+			elif(int(item[1])==5):
+				sLink = sLink + item[0]+ '=' + __decrypt(aData["cfg"]["login"]["euno"],aData["cfg"]["login"]["pepper"], key,10,12254,95369,39,21544,545555) + '&'
+			elif(int(item[1])==6):
+				sLink = sLink + item[0]+ '=' + __decrypt(aData["cfg"]["login"]["sugar"],aData["cfg"]["ads"]["lightbox2"]["time"], key,22,66595,17447,52,66852,400595) + '&'			
+ 
+		sLink = sLink + "start=0"
+
+		sMediaLink = stream_url_part1 + '&' + sLink
+
+		return [sMediaLink]
+
+def __decrypt(str, k1, k2, p4 = 11, p5 = 77213, p6 = 81371, p7 = 17, p8 = 92717, p9 = 192811):
+	tobin = hex2bin(str,len(str)*4)
+	tobin_lenght = len(tobin)
+	keys = []
+	index = 0
+
+	while (index < tobin_lenght*3):
+		k1 = ((int(k1) * p4) + p5) % p6
+		k2 = ((int(k2) * p7) + p8) % p9
+		keys.append((int(k1) + int(k2)) % tobin_lenght)
+		index += 1
+
+	index = tobin_lenght*2
+
+	while (index >= 0):
+		val1 = keys[index]
+		mod  = index%tobin_lenght
+		val2 = tobin[val1]
+		tobin[val1] = tobin[mod]
+		tobin[mod] = val2
+		index -= 1
+
+	index = 0
+	while(index < tobin_lenght):
+		tobin[index] = int(tobin[index]) ^ int(keys[index+(tobin_lenght*2)]) & 1
+		index += 1
+		decrypted = bin2hex(tobin)
+	return decrypted
+
+def hex2bin(val,fill):
+	bin_array = []
+	string =  bin(int(val, 16))[2:].zfill(fill)
+	for value in string:
+		bin_array.append(value)
+	return bin_array
+
+def bin2hex(val):
+	string = str("")
+	for char in val:
+		string+=str(char)
+	return "%x" % int(string, 2)
+
+def bin( x):
+	'''
+	bin(number) -> string
+	Stringifies an int or long in base 2.
+	'''
+	if x < 0:
+		return '-' + bin(-x)
+	out = []
+	if x == 0: out.append('0')
+	while x > 0:
+		out.append('01'[x & 1])
+		x >>= 1
+		pass
+	try:
+		return '0b' + ''.join(reversed(out))
+	except NameError, ne2:
+		out.reverse()
+	return '0b' + ''.join(out)
+
+def __get_key(nbr):
+	if nbr == '1': return 215678
+	elif nbr == '2': return 516929
+	elif nbr == '3': return 962043
+	elif nbr == '4': return 461752
+	elif nbr == '5': return 141994
+	else: return False
