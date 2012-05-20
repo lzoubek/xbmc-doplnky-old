c8ddade07c3ed5b90d80516749b559b4633b2280
Libor Zoubek <lzoubek@jezzovo.net>
2012-05-20 23:49:50 +0200

    videacesky - fixed #67, added video quality setting

--- plugin.video.videacesky.cz/default.py
+++ plugin.video.videacesky.cz/default.py
@@ -30,17 +30,26 @@ __scriptname__ = 'videacesky.cz'
 __addon__      = xbmcaddon.Addon(id=__scriptid__)
 __language__   = __addon__.getLocalizedString
 
-BASE_URL='http://www.videacesky.cz'
+BASE_URL='http://www.videacesky.cz/'
+
+
+def furl(url):
+	if url.startswith('http'):
+		return url
+	url = url.lstrip('./')
+	return BASE_URL+url
 
 def categories():
-	util.add_dir(__addon__.getLocalizedString(30001),{'top':BASE_URL+'/videozebricky/poslednich-50-videi'},util.icon('new.png'))
-	util.add_dir('Top 200',{'top':BASE_URL+'/videozebricky/top-100'},util.icon('top.png'))
+#	util.add_dir(__addon__.getLocalizedString(30001),{'top':BASE_URL+'/videozebricky/poslednich-50-videi'},util.icon('new.png'))
+	util.add_dir('Top 200',{'top':furl('/videozebricky/top-100')},util.icon('top.png'))
 	util.add_local_dir(__language__(30037),__addon__.getSetting('downloads'),util.icon('download.png'))
 	data = util.request(BASE_URL)
 	data = util.substr(data,'<ul id=\"headerMenu2\">','</ul>')
 	pattern = '<a href=\"(?P<url>[^\"]+)(.+?)>(?P<name>[^<]+)'
 	for m in re.finditer(pattern, data, re.IGNORECASE | re.DOTALL ):
-		util.add_dir(m.group('name'),{'cat':m.group('url')})
+		if m.group('url') == '/':
+			continue
+		util.add_dir(m.group('name'),{'cat':furl(m.group('url'))})
 
 def list_top(page):
 	data = util.substr(page,'<div class=\"postContent','</ul>')
@@ -48,40 +57,44 @@ def list_top(page):
 	for m in re.finditer(pattern, data, re.IGNORECASE | re.DOTALL ):
 		util.add_video(
 			m.group('name'),
-			{'play':m.group('url')},
+			{'play':furl(m.group('url'))},
 			menuItems={xbmc.getLocalizedString(33003):{'name':m.group('name'),'download':m.group('url')}}
 		)
 
-def list_content(page):
+def list_content(page,url=BASE_URL):
 	data = util.substr(page,'<div class=\"contentArea','<div class=\"pagination\">')
 	pattern = '<h\d class=\"postTitle\"><a href=\"(?P<url>[^\"]+)(.+?)<span>(?P<name>[^<]+)</span></a>(.+?)<div class=\"postContent\">[^<]+<a[^>]+[^<]+<img src=\"(?P<img>[^\"]+)[^<]+</a>[^<]*<div class=\"obs\">[^>]+>(?P<plot>(.+?))</p>'
 	for m in re.finditer(pattern, data, re.IGNORECASE | re.DOTALL ):
 		plot = re.sub('<br[^>]*>','',m.group('plot'))
 		util.add_video(
 			m.group('name'),
-			{'play':m.group('url')},
+			{'play':furl(m.group('url'))},
 			m.group('img'),
 			infoLabels={'plot':plot},
-			menuItems={xbmc.getLocalizedString(33003):{'name':m.group('name'),'download':m.group('url')}}
+			menuItems={xbmc.getLocalizedString(33003):{'name':m.group('name'),'download':furl(m.group('url'))}}
 		)
 	data = util.substr(page,'<div class=\"pagination\">','</div>')
 	m = re.search('<li class=\"info\"><span>([^<]+)',data)
 	n = re.search('<li class=\"prev\"[^<]+<a href=\"(?P<url>[^\"]+)[^<]+<span>(?P<name>[^<]+)',data)
 	k = re.search('<li class=\"next\"[^<]+<a href=\"(?P<url>[^\"]+)[^<]+<span>(?P<name>[^<]+)',data)
+	# replace last / + everyting till the end
+	myurl = re.sub('\/[\w\-]+$','/',url)
 	if not m == None:
 		if not n == None:
-			util.add_dir('%s - %s' % (m.group(1),n.group('name')),{'cat':n.group('url')})
+			util.add_dir('%s - %s' % (m.group(1),n.group('name')),{'cat':myurl+n.group('url')})
 		if not k == None:
-			util.add_dir('%s - %s' % (m.group(1),k.group('name')),{'cat':k.group('url')})
+			util.add_dir('%s - %s' % (m.group(1),k.group('name')),{'cat':myurl+k.group('url')})
 	
 def resolve(url):
 	data = util.substr(util.request(url),'<div class=\"postContent\"','</div>')
-	m = re.search('file=(?P<url>[^\&]+)',data,re.IGNORECASE | re.DOTALL)
-	if not m == None:
-		youtube.__eurl__ = 'http://www.videacesky.cz/wp-content/plugins/jw-player-plugin-for-wordpress/player.swf'
-		streams = resolver.resolve(m.group('url'))
-		if not streams == None and len(streams)>0:
-			return streams[0]
+	youtube.__eurl__ = 'http://www.videacesky.cz/wp-content/plugins/jw-player-plugin-for-wordpress/player.swf'
+	resolved = resolver.findstreams(__addon__,data,['<iframe src=\"(?P<url>[^\"]+)','\;file=(?P<url>[^\&]+)'])
+	print resolved
+	if resolved == None:
+		xbmcgui.Dialog().ok(__scriptname__,__language__(30002))
+		return
+	if not resolved == {}:
+		return resolved['url']
 
 def play(url):
 	stream = resolve(url)
@@ -111,7 +124,7 @@ if 'top' in p.keys():
 	list_top(util.request(p['top']))
 	xbmcplugin.endOfDirectory(int(sys.argv[1]))
 if 'cat' in p.keys():
-	list_content(util.request(p['cat']))
+	list_content(util.request(p['cat']),p['cat'])
 	xbmcplugin.endOfDirectory(int(sys.argv[1]))
 if 'play' in p.keys():
 	play(p['play'])
