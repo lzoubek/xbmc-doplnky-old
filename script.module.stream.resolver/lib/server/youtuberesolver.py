5195fd6c6b926b76f2dc56c005005f9275d68875
Libor Zoubek <lzoubek@jezzovo.net>
2012-11-04 22:00:14 +0100

    fixed youtuberesolver - copyied from plugin.video.youtube

--- script.module.stream.resolver/lib/server/youtuberesolver.py
+++ script.module.stream.resolver/lib/server/youtuberesolver.py
@@ -1,4 +1,126 @@
 # -*- coding: UTF-8 -*-
+'''
+   YouTube plugin for XBMC
+    Copyright (C) 2010-2012 Tobias Ussing And Henrik Mosgaard Jensen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import sys
+import urllib
+import cgi
+import simplejson as json
+
+
+class YoutubePlayer():
+    fmt_value = {
+            5: "240p",
+            18: "360p",
+            22: "720p",
+            26: "???",
+            33: "???",
+            34: "360p",
+            35: "480p",
+            37: "1080p",
+            38: "720p",
+            43: "360p",
+            44: "480p",
+            45: "720p",
+            46: "520p",
+            59: "480",
+            78: "400",
+            82: "360p",
+            83: "240p",
+            84: "720p",
+            85: "520p",
+            100: "360p",
+            101: "480p",
+            102: "720p",
+            120: "hd720",
+            121: "hd1080"
+            }
+
+    # YouTube Playback Feeds
+    urls = {}
+    urls['video_stream'] = "http://www.youtube.com/watch?v=%s&safeSearch=none"
+    urls['embed_stream'] = "http://www.youtube.com/get_video_info?video_id=%s"
+    urls['video_info'] = "http://gdata.youtube.com/feeds/api/videos/%s"
+
+    def __init__(self):
+        pass
+
+    def extractFlashVars(self, data):
+        flashvars = {}
+        found = False
+
+        for line in data.split("\n"):
+            if line.strip().startswith("var swf = \""):
+                found = True
+                p1 = line.find("=")
+                p2 = line.rfind(";")
+                if p1 <= 0 or p2 <= 0:
+                    continue
+                data = line[p1 + 1:p2]
+                break
+
+        if found:
+            data = json.loads(data)
+            data = data[data.find("flashvars"):]
+            data = data[data.find("\""):]
+            data = data[:1 + data[1:].find("\"")]
+
+            for k, v in cgi.parse_qs(data).items():
+                flashvars[k] = v[0]
+
+        return flashvars
+
+    def scrapeWebPageForVideoLinks(self, result, videoid):
+        links = {}
+
+        flashvars = self.extractFlashVars(result)
+        if not flashvars.has_key(u"url_encoded_fmt_stream_map"):
+            return (links, video)
+
+        if flashvars.has_key(u"ttsurl"):
+            video[u"ttsurl"] = flashvars[u"ttsurl"]
+
+        for url_desc in flashvars[u"url_encoded_fmt_stream_map"].split(u","):
+            url_desc_map = cgi.parse_qs(url_desc)
+
+            if not (url_desc_map.has_key(u"url") or url_desc_map.has_key(u"stream")):
+                continue
+
+            key = int(url_desc_map[u"itag"][0])
+            url = u""
+            if url_desc_map.has_key(u"url"):
+                url = urllib.unquote(url_desc_map[u"url"][0])
+            elif url_desc_map.has_key(u"stream"):
+                url = urllib.unquote(url_desc_map[u"stream"][0])
+
+            if url_desc_map.has_key(u"sig"):
+                url = url + u"&signature=" + url_desc_map[u"sig"][0]
+
+            links[key] = url
+
+        return links
+
+    def extractVideoLinksFromYoutube(self, url, videoid):
+        result = util.request(self.urls[u"video_stream"] % videoid)
+        links = self.scrapeWebPageForVideoLinks(result, videoid)
+        if len(links) == 0:
+            util.error(u"Couldn't find video url- or stream-map.")
+        return links
 #/*
 # *      Copyright (C) 2011 Libor Zoubek
 # *
@@ -21,80 +143,35 @@
 # */
 import re,util,urllib
 __name__ = 'youtube'
-__eurl__ = ''
-fmt_value = {
-        '5': '240p',
-	'17': '???',  # ???
-        '18': '360p',
-        '22': '720p',
-        '26': '???',
-        '33': '???',
-        '34': '360p',
-        '35': '480p',
-	'36': '240p',
-        '37': '1080p',
-        '38': '720p',
-        '43': '360p',
-        '44': '480p',
-        '45': '720p',
-        '46': '520p',
-        '59': '480p',
-        '78': '400p',
-        '82': '360p',
-        '83': '240p',
-        '84': '720p',
-        '85': '520p',
-        '100': '360p',
-        '101': '480p',
-        '102': '720p'
-        }
 
 
 def supports(url):
-	return not _regex(url) == None
+    return not _regex(url) == None
 
 def resolve(url):
-	m = _regex(url)
-	if not m == None:
-		request = urllib.urlencode({'video_id':m.group('id'),'el':'embedded','asv':'3','hl':'en_US','eurl':__eurl__})
-		data = util.request('http://www.youtube.com/get_video_info?%s' % request)
-		data = urllib.unquote(util.decode_html(data))
-		if data.find('status=fail') > -1:
-			util.error('youtube resolver failed: '+data+' videoid:'+m.group('id'))
-		else:
-			# to avoid returning more than 1 url of same quality
-			qualities = []
-			resolved = []
-			for n in re.finditer('itag=(?P<q>\d+)\&url=(?P<url>.+?)\&quality',data):
-				stream = urllib.unquote(n.group('url'))
-				quality = '???'
-				if n.group('q') in fmt_value.keys():
-					quality = fmt_value[n.group('q')]
-				if not quality in qualities:
-					item = {}
-					item['name'] = __name__
-					item['url'] = stream
-					item['quality'] = quality
-					item['surl'] = url
-					item['subs'] = ''
-					qualities.append(quality)
-					resolved.append(item)
-			return resolved
-		
+    m = _regex(url)
+    if not m == None:
+        player = YoutubePlayer()
+        links = player.extractVideoLinksFromYoutube(url,m.group('id'))
+        resolved = []
+        for q in links:
+            if q in player.fmt_value.keys():
+                quality = player.fmt_value[q]
+                item = {}
+                item['name'] = __name__
+                item['url'] = links[q]
+                item['quality'] = quality
+                item['surl'] = url
+                item['subs'] = ''
+        resolved.append(item)
+        return resolved
+
 # returns the steam url
 def url(url):
-	m = _regex(url)
-	if not m == None:
-		request = urllib.urlencode({'video_id':m.group('id'),'el':'embedded','asv':'3','hl':'en_US','eurl':__eurl__})
-		data = util.request('http://www.youtube.com/get_video_info?%s' % request)
-		data = urllib.unquote(util.decode_html(data))
-		
-		if data.find('status=fail') > -1:
-			util.error('youtube resolver failed: '+data+' videoid:'+m.group('id'))
-		else:
-			stream = re.search('url_encoded_fmt_stream_map=url=(.+?)fallback_host',data,re.IGNORECASE | re.DOTALL).group(1)			
-			return [urllib.unquote(stream)]
+    m = _regex(url)
+    if not m == None:
+        [r['url'] for r in resolve(url)]
 
 def _regex(url):
-	return re.search('https?\://www\.youtube\.com/(watch\?v=|v/|embed/)(?P<id>.+?)(\?|$|&)',url,re.IGNORECASE | re.DOTALL)
+    return re.search('https?\://www\.youtube\.com/(watch\?v=|v/|embed/)(?P<id>.+?)(\?|$|&)',url,re.IGNORECASE | re.DOTALL)
 
