25600b818617fcda0f94fb7bd0692b73b8f17a72
Libor Zoubek <lzoubek@jezzovo.net>
2012-11-07 21:28:38 +0100

    fixed indent errrors

--- script.usage.tracker/default.py
+++ script.usage.tracker/default.py
@@ -101,19 +101,19 @@ def register(params):
 
 def params():
     param={}
-        paramstring=sys.argv[2]
-        if len(paramstring)>=2:
-            params=sys.argv[2]
-                cleanedparams=params.replace('?','')
-                if (params[len(params)-1]=='/'):
-                    params=params[0:len(params)-2]
-                pairsofparams=cleanedparams.split('&')
-                param={}
-                for i in range(len(pairsofparams)):
-                    splitparams={}
-                        splitparams=pairsofparams[i].split('=')
-                        if (len(splitparams))==2:
-                            param[splitparams[0]]=splitparams[1]
+    paramstring=sys.argv[2]
+    if len(paramstring)>=2:
+        params=sys.argv[2]
+        cleanedparams=params.replace('?','')
+        if (params[len(params)-1]=='/'):
+            params=params[0:len(params)-2]
+        pairsofparams=cleanedparams.split('&')
+        param={}
+        for i in range(len(pairsofparams)):
+            splitparams={}
+            splitparams=pairsofparams[i].split('=')
+            if (len(splitparams))==2:
+                param[splitparams[0]]=splitparams[1]
     return param
 
 def printHelp():
