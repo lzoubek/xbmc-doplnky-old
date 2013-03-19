ce26c67ed28db9d92d6d05d8d14e2954b92c4641
Libor Zoubek <lzoubek@jezzovo.net>
2013-03-19 20:39:11 +0100

    fixed koukni resolver to work with rtmp, but xbmc 12 is not able to play such streams

--- script.module.stream.resolver/lib/contentprovider/koukni.py
+++ script.module.stream.resolver/lib/contentprovider/koukni.py
@@ -24,81 +24,79 @@ from provider import ContentProvider
 
 class KoukniContentProvider(ContentProvider):
 
-	def __init__(self,username=None,password=None,filter=None):
-		ContentProvider.__init__(self,'koukni.cz','http://koukni.cz/',username,password,filter)
-		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.LWPCookieJar()))
-		urllib2.install_opener(opener)
+    def __init__(self,username=None,password=None,filter=None):
+        ContentProvider.__init__(self,'koukni.cz','http://koukni.cz/',username,password,filter)
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.LWPCookieJar()))
+        urllib2.install_opener(opener)
 
-	def capabilities(self):
-		return ['search','resolve','cagegories']
+    def capabilities(self):
+        return ['search','resolve','cagegories']
 
-	def search(self,keyword):
-		return self.list('hledej?hledej='+urllib.quote(keyword))
+    def search(self,keyword):
+        return self.list('hledej?hledej='+urllib.quote(keyword))
 
-	def categories(self):
-		result = []
-		
-		item = self.dir_item()
-		item['type'] = 'new'
-		item['url'] = self._url('new')
-		result.append(item)
-		
-		item = self.dir_item()
-		item['type'] = 'top'
-		item['url'] = self._url('nej')
-		result.append(item)
+    def categories(self):
+        result = []
 
-		for cat in ['clipz','Wa4lock','minno','USA','simpsons','serialz','znovy','zoufalky','vypravej','topgear','COWCOOSH']:
-			item = self.dir_item()
-			item['title'] = cat
-			item['url'] = self._url(cat)
-			result.append(item)
-		return result
-		
-	def list(self,url):
-		result = []
-		data = util.request(self._url(url))
-		for m in re.finditer('<div class=\"row\"(.+?)<a href=\"(?P<url>[^\"]+)(.+?)src=\"(?P<logo>[^\"]+)(.+?)<h1>(?P<name>[^<]+)',data,re.IGNORECASE | re.DOTALL ):
-			item = self.video_item()
-			item['title'] = m.group('name')
-			item['img'] = self._url(m.group('logo'))
-			item['url'] = m.group('url')
-			self._filter(result,item)
-		navurl = url
-		index = url.find('?')
-		if index > 0:
-			navurl = url[:index]
-	
-		prev = re.search('<a href=\"(?P<url>[^\"]+)\">[^<]*<img src=\"\./style/images/predchozi.png',data,re.IGNORECASE | re.DOTALL )
-		if prev:
-			item = self.dir_item()
-			item['type'] = 'prev'
-			item['url'] = navurl+prev.group('url')
-			result.append(item)
-		next = re.search('<a href=\"(?P<url>[^\"]+)\">[^<]*<img src=\"\./style/images/dalsi.png',data,re.IGNORECASE | re.DOTALL )
-		if next:
-			item = self.dir_item()
-			item['type'] = 'next'
-			item['url'] = navurl+next.group('url')
-			result.append(item)
-		return result
+        item = self.dir_item()
+        item['type'] = 'new'
+        item['url'] = self._url('new')
+        result.append(item)
+
+        item = self.dir_item()
+        item['type'] = 'top'
+        item['url'] = self._url('nej')
+        result.append(item)
+
+        for cat in ['clipz','Wa4lock','minno','USA','simpsons','serialz','znovy','zoufalky','vypravej','topgear','COWCOOSH']:
+            item = self.dir_item()
+            item['title'] = cat
+            item['url'] = self._url(cat)
+            result.append(item)
+        return result
+
+    def list(self,url):
+        result = []
+        data = util.request(self._url(url))
+        for m in re.finditer('<div class=\"row\"(.+?)<a href=\"(?P<url>[^\"]+)(.+?)src=\"(?P<logo>[^\"]+)(.+?)<h1>(?P<name>[^<]+)',data,re.IGNORECASE | re.DOTALL ):
+            item = self.video_item()
+            item['title'] = m.group('name')
+            item['img'] = self._url(m.group('logo'))
+            item['url'] = m.group('url')
+            self._filter(result,item)
+        navurl = url
+        index = url.find('?')
+        if index > 0:
+            navurl = url[:index]
+
+        prev = re.search('<a href=\"(?P<url>[^\"]+)\">[^<]*<img src=\"\./style/images/predchozi.png',data,re.IGNORECASE | re.DOTALL )
+        if prev:
+            item = self.dir_item()
+            item['type'] = 'prev'
+            item['url'] = navurl+prev.group('url')
+            result.append(item)
+        next = re.search('<a href=\"(?P<url>[^\"]+)\">[^<]*<img src=\"\./style/images/dalsi.png',data,re.IGNORECASE | re.DOTALL )
+        if next:
+            item = self.dir_item()
+            item['type'] = 'next'
+            item['url'] = navurl+next.group('url')
+            result.append(item)
+        return result
 
 
 
-	def resolve(self,item,captcha_cb=None,select_cb=None):
-		item = item.copy()
-		url = self._url(item['url'])
-		data = util.request(url)
-		video = re.search('=\"player\" href=\"(?P<url>[^\"]+)',data,re.IGNORECASE | re.DOTALL)
-		subs = re.search('captionUrl\: \'(?P<url>[^\']+)',data,re.IGNORECASE | re.DOTALL)
-		if video:
-			req = urllib2.Request(self._url(video.group('url')))
-			req.add_header('User-Agent',util.UA)
-			response = urllib2.urlopen(req)
-			response.close()
-			item['url'] = response.geturl()
-			item['surl'] = url
-			item['quality'] = '720p'
-			if subs:
-				item['subs'] = self._url(subs.group('url'))
-			return item
+    def resolve(self,item,captcha_cb=None,select_cb=None):
+        item = item.copy()
+        url = self._url(item['url'])
+        data = util.request(url)
+        video = re.search('clip.+?url\: \'(?P<url>[^\']+)',data,re.IGNORECASE | re.DOTALL)
+        conn = re.search('netConnectionUrl\: \'(?P<url>[^\']+)',data,re.IGNORECASE | re.DOTALL)
+        subs = re.search('captionUrl\: \'(?P<url>[^\']+)',data,re.IGNORECASE | re.DOTALL)
+        if video and conn:
+            item['url'] = '%s/%s' % (conn.group('url'),video.group('url'))
+            item['surl'] = url
+            item['quality'] = '720p'
+            if subs:
+                item['subs'] = self._url(subs.group('url'))
+            print item
+            return item
