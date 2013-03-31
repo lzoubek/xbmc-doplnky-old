f23ece3081021c941e81283c352ee35c64c4f628
Libor Zoubek <lzoubek@jezzovo.net>
2013-03-31 23:37:32 +0200

    added befun tests

--- test/testproviders.py
+++ test/testproviders.py
@@ -3,7 +3,7 @@ import unittest
 filename = os.path.dirname( os.path.realpath(__file__) )
 sys.path.append( os.path.join ( filename,'..' ) )
 import providertestcase
-import eserial
+import eserial,befun
 
 
 class EserialProviderTestCase(providertestcase.ProviderTestCase):
@@ -14,3 +14,13 @@ class EserialProviderTestCase(providertestcase.ProviderTestCase):
         self.list_urls=['#show#http://www.eserial.cz/alcatraz/','#list#http://www.eserial.cz/anatomie-lzi/serie/2']
         self.resolve_items = [{'url':'http://www.eserial.cz/anatomie-lzi/video/3673'}]
         self.categories_list = True
+
+class BefunProviderTestCase(providertestcase.ProviderTestCase):
+
+    def setUp(self):
+        self.provider_class = befun.BefunContentProvider
+        self.cp = self.provider_class()
+        self.list_urls=['#cat#serialy','#cat#filmy','#cat#filmy/komedie']
+        self.resolve_items = [{'url':'http://www.befun.cz/film/1974/avengers-hd-munk'}]
+        self.categories_list = True
+        self.search_keywords = ['avengers']
