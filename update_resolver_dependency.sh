9d5a2b1a60d18150ee57e79c0d13f9ed31ea326b
Libor Zoubek <lzoubek@jezzovo.net>
2013-04-05 18:59:22 +0200

    bump stream.resolver dependencies

--- update_resolver_dependency.sh
+++ update_resolver_dependency.sh
@@ -1,1 +1,2 @@
-find . -name 'addon.xml' | xargs sed -i 's;addon=\"script.module.stream.resolver.*$;addon="script.module.stream.resolver" version="1.5.0" />;'
+find . -name 'addon.xml' | xargs sed -i 's;addon=\"script.module.stream.resolver.*$;addon="script.module.stream.resolver" version="1.5.4" />;'
+#find . -name 'addon.xml' | xargs sed -i 's;addon=\"xbmc\.python.*$;addon="xbmc.python" version="2.0" />;'
