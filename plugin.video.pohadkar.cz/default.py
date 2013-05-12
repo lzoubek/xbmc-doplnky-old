f7527a53e3f36ed899f9d6f22f33b9149b39e670
Libor Zoubek <lzoubek@jezzovo.net>
2013-05-12 11:12:36 +0200

    pohadkar.cz lives again!

--- plugin.video.pohadkar.cz/default.py
+++ plugin.video.pohadkar.cz/default.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 #/*
-# *      Copyright (C) 2011 Libor Zoubek
+# *      Copyright (C) 2013 Libor Zoubek
 # *
 # *
 # *  This Program is free software; you can redistribute it and/or modify
@@ -19,198 +19,63 @@
 # *  http://www.gnu.org/copyleft/gpl.html
 # *
 # */
-
-import re,os,urllib,urllib2,md5
-import xbmcaddon,xbmc,xbmcgui,xbmcplugin
-
+import os
+import xbmc,xbmcaddon,xbmcgui,xbmcplugin,util,xbmcprovider,xbmcutil
 __scriptid__   = 'plugin.video.pohadkar.cz'
-__scriptname__ = 'pohadkar.cz'
+__scriptname__ = 'Pohádkář.cz'
 __addon__      = xbmcaddon.Addon(id=__scriptid__)
 __language__   = __addon__.getLocalizedString
 
 sys.path.append( os.path.join ( __addon__.getAddonInfo('path'), 'resources','lib') )
+import pohadkar
+settings = {'downloads':__addon__.getSetting('downloads'),'quality':__addon__.getSetting('quality')}
 
-import util,resolver,search
-import youtuberesolver as youtube
+class PohadkarContentProvider(xbmcprovider.XBMCMultiResolverContentProvider):
 
+    def play(self,url):
+        stream = self.resolve(url)
+        print type(stream)
+        if type(stream) == type([]):
+            # resolved to mutliple files, we'll feed playlist and play the first one
+            playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+            playlist.clear()
+            for video in stream:
+                li = xbmcgui.ListItem(label=video['title'],path=video['url'],iconImage='DefaultVideo.png')
+                playlist.add(video['url'],li)
+            stream = stream[0]
+        if stream:
+            xbmcutil.reportUsage(self.addon_id,self.addon_id+'/play')
+            if 'headers' in stream.keys():
+                for header in stream['headers']:
+                    stream['url'] += '|%s=%s' % (header,stream['headers'][header])
+            print 'Sending %s to player' % stream['url']
+            li = xbmcgui.ListItem(path=stream['url'],iconImage='DefaulVideo.png')
+            xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, li)
+            xbmcutil.load_subtitles(stream['subs'])
 
-BASE_URL='http://www.pohadkar.cz/'
+    def resolve(self,url):
+        def select_cb(resolved):
+            # for this plugin we do not care about quality at all
+            if len(resolved) == 1:
+                return resolved[0]
+            dialog = xbmcgui.Dialog()
+            opts = [__language__(30050)]
+            opts = opts + ['%s [%s]'%(r['title'],r['quality']) for r in resolved]
+            ret = dialog.select(xbmcutil.__lang__(30005), opts)
+            if ret == 0:
+                # requested to play all streams in given order - so return them all
+                return resolved
+            if ret >= 0:
+                return resolved[ret]
 
-def _search_cb(what):
-	print 'searching for '+what
+        item = self.provider.video_item()
+        item.update({'url':url})
+        try:
+            return self.provider.resolve(item,select_cb=select_cb)
+        except ResolveException, e:
+            self._handle_exc(e)
 
-def furl(url):
-	if url.startswith('http'):
-		return url
-	url = url.lstrip('./')
-	return BASE_URL+url
-
-def _get_meta(name,link):
-	# load meta from disk or download it (slow for each tale, thatwhy we cache it)
-	local = xbmc.translatePath(__addon__.getAddonInfo('profile'))
-	if not os.path.exists(local):
-		os.makedirs(local)
-	m = md5.new()
-	m.update(name)
-	image = os.path.join(local,m.hexdigest()+'_img.png')
-	plot = os.path.join(local,m.hexdigest()+'_plot.txt')
-	if not os.path.exists(image):
-		data = util.request(link)
-		_get_image(data,image)
-		_get_plot(data,plot)
-	return image,_load(plot)
-
-
-def _save(data,local):
-	print 'Saving file %s' % local
-	f = open(local,'w')
-	f.write(data)
-	f.close()
-
-def _load(file):
-	if not os.path.exists(file):
-		return ''
-	f = open(file,'r')
-	data = f.read()
-	f.close()
-	return data
-
-def _get_plot(data,local):
- 		data = util.substr(data,'<div id=\"tale_description\"','<div class=\"cleaner')
-		p = data
-		p = re.sub('<div[^>]+>','',p)
-		p = re.sub('<table.*','',p)
-		p = re.sub('</span>|<br[^>]*>|<ul>|</ul>|<hr[^>]*>','',p)
-		p = re.sub('<span[^>]*>|<p[^>]*>|<li[^>]*>','',p)
-		p = re.sub('<strong>|<a[^>]*>|<h[\d]+>','[B]',p)
-		p = re.sub('</strong>|</a>|</h[\d]+>','[/B]',p)
-		p = re.sub('</p>|</li>','[CR]',p)
-		p = re.sub('<em>','[I]',p)
-		p = re.sub('</em>','[/I]',p)
-		p = re.sub('<img[^>]+>','',p)
-		p = re.sub('\[B\]Edituj popis\[\/B\]','',p)
-		p = re.sub('\[B\]\[B\]','[B]',p)
-		p = re.sub('\[/B\]\[/B\]','[/B]',p)
-		p = re.sub('\[B\][ ]*\[/B\]','',p)
-		_save(util.decode_html(''.join(p)).encode('utf-8'),local)
-
-
-def _get_image(data,local):
-		m = re.search('<img id=\"tale_picture\" src=\"(?P<img>[^\"]+)', data, re.IGNORECASE | re.DOTALL)
-		if not m == None:
-			img = furl(m.group('img'))
-			print ' Downloading %s' % img
-			_save(util.request(img),local)
-
-def list_all():
-	for letter in letters:
-		data = util.request(BASE_URL+'system/load-vypis/?znak='+letter+'&typ=1&zar=hp')
-		pattern = '<a href=\"(?P<url>[^\"]+)[^>]+>(?P<name>[^<]+)'
-		for m in re.finditer(pattern,data,re.IGNORECASE | re.DOTALL ):
-			image,plot = _get_meta(m.group('name'),furl(m.group('url')))
-			util.add_dir(m.group('name'),{'tale':furl(m.group('url')+'video/')},image,infoLabels={'Plot':plot})
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-def list_letter(index):
-	letter = letters[int(index)]
-	data = util.request(BASE_URL+'system/load-vypis/?znak='+letter+'&typ=1&zar=hp')
-	pattern = '<a href=\"(?P<url>[^\"]+)[^>]+>(?P<name>[^<]+)'
-	for m in re.finditer(pattern,data,re.IGNORECASE | re.DOTALL ):
-			image,plot = _get_meta(m.group('name'),furl(m.group('url')))
-			util.add_dir(m.group('name'),{'tale':furl(m.group('url')+'video/')},image,infoLabels={'Plot':plot})
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-def categories():
-	#search.item()
-	util.add_local_dir(__language__(30037),__addon__.getSetting('downloads'),util.icon('download.png'))
-	util.add_dir('Všechny',{'list-all':''})
-	for index,letter in enumerate(letters):
-		util.add_dir(letter,{'list':str(index)})
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-
-def list_page(url):
-	page = util.request(url)
-	return parse_page(page,url)
-
-def parse_page(page,url):
-	data = util.substr(page,'<div class=\"vypis','<div class=\"right')
-	pattern = '<div class=\"tale_char_div\"(.+?)<img(.+?)src=\"(?P<img>[^\"]+)(.+?)<a(.+?)href=\"(?P<url>[^\"]+)[^>]+>(?P<name>[^<]+)<(.+?)<p[^>]*>(?P<plot>[^<]+)'
-	for m in re.finditer(pattern, data, re.IGNORECASE | re.DOTALL):
-		util.add_video(
-			m.group('name'),
-			{'play':furl(m.group('url'))},
-			m.group('img'),
-			infoLabels={'Plot':m.group('plot')},
-			menuItems={xbmc.getLocalizedString(33003):{'name':m.group('name'),'download':furl(m.group('url'))}}
-		)
-	data = util.substr(page,'<p class=\"p_wrapper','</p>')
-	index =  url.find('?')
-	if index > 0:
-		url = url[:index]
-	n = re.search('<a(.+?)href=\"(?P<url>[^\"]+)\"[^>]*>&gt;<',data)
-	if n:
-		util.add_dir(__language__(30012),{'tale':furl(url+n.group('url'))},util.icon('next.png'))
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-def play(url):
-	stream = resolve(url)
-	if stream:
-		util.reportUsage(__scriptid__,__scriptid__+'/play')
-		print 'Sending %s to player' % stream
-		if len(stream) > 1:
-			playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
-			playlist.clear()
-			for st in stream:
-				li = xbmcgui.ListItem(path=st,iconImage='DefaultVideo.png')
-				playlist.add(st,li)
-		li = xbmcgui.ListItem(path=stream[0],iconImage='DefaulVideo.png')
-		return xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, li)
-
-def resolve(url):
-	page = util.request(url)
-	data = util.substr(page,'<div id=\"video','<div id=\"controller')
-	youtube.__eurl__ = url
-	resolved = resolver.findstreams_multi(__addon__,data,['<embed( )src=\"(?P<url>[^\"]+)','<object(.+?)data=\"(?P<url>[^\"]+)','<iframe(.+?)src=\"(?P<url>[^\"]+)'])
-	if resolved == None:
-		xbmcgui.Dialog().ok(__scriptname__,__language__(30001))
-		return
-	if not resolved == []:
-		return [r['url'] for r in resolved]
-
-def download(url,name):
-	downloads = __addon__.getSetting('downloads')
-	if '' == downloads:
-		xbmcgui.Dialog().ok(__scriptname__,__language__(30031))
-		return
-	stream = resolve(url)
-	if stream:
-		util.reportUsage(__scriptid__,__scriptid__+'/download')
-		if len(stream) > 1:
-			index=0
-			for st in stream:
-				index+=1
-				filename = name+'_part'+str(index)
-				if index == len(stream)-1:
-					util.download(__addon__,filename,st,os.path.join(downloads,filename+'.mp4'))
-				else:
-					util.download(__addon__,filename,st,os.path.join(downloads,filename+'.mp4'),notifyFinishDialog=False)
-		else:
-			util.download(__addon__,name,stream[0],os.path.join(downloads,name+'.mp4'))
-
-p = util.params()
-letters = ['A','B','C','Č','D','Ď','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','Ř','S','Š','T','Ť','U','V','W','X','Y','Z','Ž']
-if p=={}:
-	xbmc.executebuiltin('RunPlugin(plugin://script.usage.tracker/?do=reg&cond=31000&id=%s)' % __scriptid__)
-	categories()
-if 'list-all' in p.keys():
-	list_all()
-if 'list' in p.keys():
-	list_letter(p['list'])
-if 'tale' in p.keys():
-	list_page(p['tale'])
-if 'play' in p.keys():
-	play(p['play'])
-if 'download' in p.keys():
-	download(p['download'],p['name'])
-search.main(__addon__,'search_history',p,_search_cb)
+params = util.params()
+if params=={}:
+	xbmcutil.init_usage_reporting( __scriptid__)
+PohadkarContentProvider(pohadkar.PohadkarContentProvider(tmp_dir=xbmc.translatePath(__addon__.getAddonInfo('profile'))),settings,__addon__).run(params)
---
+++ plugin.video.pohadkar.cz/resources/lib/pohadkar.py
@@ -0,0 +1,167 @@
+# -*- coding: UTF-8 -*-
+#/*
+# *      Copyright (C) 2013 Libor Zoubek
+# *
+# *
+# *  This Program is free software; you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2, or (at your option)
+# *  any later version.
+# *
+# *  This Program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with this program; see the file COPYING.  If not, write to
+# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# *  http://www.gnu.org/copyleft/gpl.html
+# *
+# */
+
+LETTERS = ['A','B','C','Č','D','Ď','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','Ř','S','Š','T','Ť','U','V','W','X','Y','Z','Ž']
+import re,urllib,urllib2,cookielib,random,util,sys,os,traceback
+from provider import ContentProvider
+from provider import ResolveException
+import md5
+import resolver
+class PohadkarContentProvider(ContentProvider):
+
+    def __init__(self,username=None,password=None,filter=None,tmp_dir='.'):
+        ContentProvider.__init__(self,'pohadkar.cz','http://www.pohadkar.cz/',username,password,filter)
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.LWPCookieJar()))
+        urllib2.install_opener(opener)
+        self.tmp_dir = tmp_dir
+
+    def capabilities(self):
+        return ['resolve','cagegories','!download']
+
+    def search(self,keyword):
+        return self.list('hledej?hledej='+urllib.quote(keyword))
+
+    def categories(self):
+        result = []
+        #item = self.dir_item()
+        #item['url'] = '#list-all#'
+        #item['title'] = 'Všechny pohádky'
+        #result.append(item)
+        for index,letter in enumerate(LETTERS):
+            item = self.dir_item()
+            item['title'] = letter
+            item['url'] = '#list#%d' % index
+            result.append(item)
+        return result
+
+    def list(self,url):
+        if url.find('#list-all#') == 0:
+            return self.list_all()
+        if url.find('#list#') == 0:
+            return self.list_letter(url[6:])
+        else:
+            return self.parse_page(util.request(self._url(url)),self._url(url))
+
+    def list_all(self):
+        result = []
+        for index,letter in enumerate(LETTERS):
+            result = result + self.list_letter(index)
+        return result
+
+    def parse_page(self,page,url):
+        data = util.substr(page,'<div class=\"vypis','<div class=\"right')
+        pattern = '<div class=\"tale_char_div\"(.+?)<img(.+?)src=\"(?P<img>[^\"]+)(.+?)<a(.+?)href=\"(?P<url>[^\"]+)[^>]+>(?P<name>[^<]+)<(.+?)<p[^>]*>(?P<plot>[^<]+)'
+        result = []
+        for m in re.finditer(pattern, data, re.IGNORECASE | re.DOTALL):
+            item = self.video_item()
+            item['title'] = m.group('name')
+            item['url'] = m.group('url')
+            item['img'] = m.group('img')
+            item['plot'] = m.group('plot')
+            self._filter(result,item)
+        data = util.substr(page,'<p class=\"p_wrapper','</p>')
+        index =  url.find('?')
+        if index > 0:
+            url = url[:index]
+        n = re.search('<a(.+?)href=\"(?P<url>[^\"]+)\"[^>]*>&gt;<',data)
+        if n:
+            item = self.dir_item()
+            item['type'] = 'next'
+            item['url'] = self._url(url+n.group('url'))
+        return result
+
+    def list_letter(self,index):
+        letter = LETTERS[int(index)]
+        data = util.request(self.base_url+'system/load-vypis/?znak='+letter+'&typ=1&zar=hp')
+        pattern = '<a href=\"(?P<url>[^\"]+)[^>]+>(?P<name>[^<]+)'
+        result = []
+        for m in re.finditer(pattern,data,re.IGNORECASE | re.DOTALL ):
+            image,plot = self._get_meta(m.group('name'),self._url(m.group('url')))
+            item = self.dir_item()
+            item['title'] = m.group('name')
+            item['url'] = m.group('url')+'video/'
+            item['img'] = image
+            item['plot'] = plot
+            self._filter(result,item)
+        return result
+
+    def _get_meta(self,name,link):
+        # load meta from disk or download it (slow for each tale, thatswhy we cache it)
+        local = self.tmp_dir
+        if not os.path.exists(local):
+            os.makedirs(local)
+        m = md5.new()
+        m.update(name)
+        image = os.path.join(local,m.hexdigest()+'_img.png')
+        plot = os.path.join(local,m.hexdigest()+'_plot.txt')
+        if not os.path.exists(image):
+            data = util.request(link)
+            self._get_image(data,image)
+            self._get_plot(data,plot)
+        return image,util.read_file(plot)
+
+    def _get_plot(self,data,local):
+        data = util.substr(data,'<div id=\"tale_description\"','<div class=\"cleaner')
+        p = data
+        p = re.sub('<div[^>]+>','',p)
+        p = re.sub('<table.*','',p)
+        p = re.sub('</span>|<br[^>]*>|<ul>|</ul>|<hr[^>]*>','',p)
+        p = re.sub('<span[^>]*>|<p[^>]*>|<li[^>]*>','',p)
+        p = re.sub('<strong>|<a[^>]*>|<h[\d]+>','[B]',p)
+        p = re.sub('</strong>|</a>|</h[\d]+>','[/B]',p)
+        p = re.sub('</p>|</li>','[CR]',p)
+        p = re.sub('<em>','[I]',p)
+        p = re.sub('</em>','[/I]',p)
+        p = re.sub('<img[^>]+>','',p)
+        p = re.sub('\[B\]Edituj popis\[\/B\]','',p)
+        p = re.sub('\[B\]\[B\]','[B]',p)
+        p = re.sub('\[/B\]\[/B\]','[/B]',p)
+        p = re.sub('\[B\][ ]*\[/B\]','',p)
+        util.save_data_to_file(util.decode_html(''.join(p)).encode('utf-8'),local)
+
+
+    def _get_image(self,data,local):
+        m = re.search('<img id=\"tale_picture\" src=\"(?P<img>[^\"]+)', data, re.IGNORECASE | re.DOTALL)
+        if not m == None:
+            img = self._url(m.group('img'))
+            util.save_data_to_file(util.request(img),local)
+
+    def resolve(self,item,captcha_cb=None,select_cb=None):
+        page = util.request(self._url(item['url']))
+        data = util.substr(page,'<div id=\"video','<div id=\"controller')
+        resolved = resolver.findstreams(data,['<embed( )src=\"(?P<url>[^\"]+)','<object(.+?)data=\"(?P<url>[^\"]+)','<iframe(.+?)src=\"(?P<url>[^\"]+)'])
+        result = []
+        if not resolved:
+            raise ResolveException('Video nenalezeno')
+        for i in resolved:
+            item = self.video_item()
+            item['title'] = i['title']
+            item['url'] = i['url']
+            item['quality'] = i['quality']
+            item['surl'] = i['surl']
+            item['headers'] = i['headers']
+            result.append(item)
+        result = sorted(result,key=lambda x:x['title'])
+        if len(result)==1:
+            return result[0]
+        elif len(result) > 1 and select_cb:
+            return select_cb(result)
