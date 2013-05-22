3ed057b3a7431ac1e0978628785d7e4d53949411
Libor Zoubek <lzoubek@jezzovo.net>
2013-05-22 23:34:01 +0200

    updated to stream.resolver 1.6.0

--- update_resolver_dependency.sh
+++ update_resolver_dependency.sh
@@ -1,2 +1,2 @@
-find . -name 'addon.xml' | xargs sed -i 's;addon=\"script.module.stream.resolver.*$;addon="script.module.stream.resolver" version="1.5.7" />;'
+find . -name 'addon.xml' | xargs sed -i 's;addon=\"script.module.stream.resolver.*$;addon="script.module.stream.resolver" version="1.6.0" />;'
 #find . -name 'addon.xml' | xargs sed -i 's;addon=\"xbmc\.python.*$;addon="xbmc.python" version="2.0" />;'
