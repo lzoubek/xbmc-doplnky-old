fa1e6d5b629b109aed521a30c665a56f5da9455e
Libor Zoubek <lzoubek@redhat.com>
2013-10-10 21:49:45 +0200

    added webshare test

---
+++ test/plugin.video.online-files/testwebshare.py
@@ -0,0 +1,20 @@
+import sys,os,urllib2
+import unittest
+filename = os.path.dirname( os.path.realpath(__file__) )
+sys.path.append( os.path.join ( filename,'..' ) )
+from providertestcase import ProviderTestCase
+
+from webshare import WebshareContentProvider
+
+class WebshareProviderTestCase(ProviderTestCase):
+
+    def setUp(self):
+        self.provider_class = WebshareContentProvider
+        self.cp = self.provider_class()
+        self.list_urls=[]
+        self.resolve_items = [{'url':'ident=1de10F12i7'}]
+        #self.resolve_items = [{'url':'http://www.fastshare.cz/2050612/avengers-avengers-the-2012-dvdrip-xvid-cz.avi'}]
+
+        self.search_keywords = ['avengers']
+        self.categories_list = False
+
