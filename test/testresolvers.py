56cdecfe65d4198dc3243619cb630232347f7e0e
Libor Zoubek <lzoubek@redhat.com>
2013-11-22 00:16:39 +0100

    added few more resolver tests

--- test/testresolvers.py
+++ test/testresolvers.py
@@ -10,3 +10,17 @@ class StreaminTo(ResolverTestCase):
     def assertions(self,url,results):
         for result in results:
             self.assertTrue(result['quality'] == '360p','Quality is correctly detected')
+
+import youtuberesolver
+class Youtube(ResolverTestCase):
+    
+    def setUp(self):
+        self.resolver = youtuberesolver
+        self.urls=['http://www.youtube.com/watch?v=KKbUP7brxmY','//www.youtube.com/embed/jY3VBnG1dX4']
+
+import anyfilesresolver
+class AnyFiles(ResolverTestCase):
+    
+    def setUp(self):
+        self.resolver = anyfilesresolver
+        self.urls=['http://video.anyfiles.pl/w.jsp?id=85590&#038;width=620&#038;height=390&#038;pos=0&#038;skin=1']
