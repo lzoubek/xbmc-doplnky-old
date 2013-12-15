fa93907e279f910751488a927034e72becb9b20a
Libor Zoubek <lzoubek@redhat.com>
2013-12-15 19:39:54 +0100

    replace update_resolver_dependency script by smarter bump_addon which bumps addon version & takes care about latest dependency versions

--- update_resolver_dependency.sh
+++
@@ -1,2 +0,0 @@
-find . -name 'addon.xml' | xargs sed -i 's;addon=\"script.module.stream.resolver.*$;addon="script.module.stream.resolver" version="1.6.6" />;'
-#find . -name 'addon.xml' | xargs sed -i 's;addon=\"xbmc\.python.*$;addon="xbmc.python" version="2.0" />;'
