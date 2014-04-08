2b9f3e51a3f1d24cb6f12675f210f5900b05a94a
mx3L <mx3ldev@gmail.com>
2014-04-08 21:34:34 +0200

    fixed anyfiles resolver

--- script.module.stream.resolver/lib/server/anyfilesresolver.py
+++ script.module.stream.resolver/lib/server/anyfilesresolver.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 # *  GNU General Public License for more details.
 # *
-# * 
+# *
 # *  You should have received a copy of the GNU General Public License
 # *  along with this program; see the file COPYING.  If not, write to
 # *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -76,47 +76,29 @@ def _decode(param):
 
     return loc_2
 
-
 def resolve(url):
     m = _regex(url)
     if m:
-        req = urllib2.Request(url)
-        resp = urllib2.urlopen(req)
+        resp = urllib2.urlopen(url)
         sessc = resp.headers.get('Set-Cookie').split(';')[0]
         resp.close()
         furl = "%s/w.jsp?id=%s&width=620&height=349&pos=&skin=0" % (BASE_URL,m.group('id'))
         headers = {'Cookie':sessc, 'Referer':url}
         data = util.request(furl,headers)
         m1 = re.search('document.cookie = "([^"]+?)"',data)
-        m2 = re.search("""var flashvars = {.+?"st":"(.+?)"}""",data)
-        m3 = re.search('swfobject\.embedSWF\(\"(\/uppod\/uppod\.swf\?rand=\d+)\"',data, re.DOTALL)
-        if m1 and m2 and m3:
+        m2 = re.search('var flashvars = {[^"]+?config: "([^"]+?)" }',data)
+        if m1 and m2:
             headers['Cookie'] = headers['Cookie'] + '; ' + m1.group(1)
-            headers['Referer'] = BASE_URL + m3.group(1)
-            nUrl = _decode(m2.group(1)[2:]).encode('utf-8').strip()
-            if 'http://' in nUrl: url2 = nUrl
-            else: url2 = 'http://video.anyfiles.pl' + nUrl
-            rand = "&rand=%s"%str(_gen_random_decimal(0,9999999999999999))
-            data = util.request(url2+ "&ref=" +urllib.quote_plus(url)+rand, headers)
-            data = _decode(data).encode('utf-8').strip()
-            while data[-2:] != '"}': data = data[:-1]
-            try:
-                result = util.json.loads(data)
-            except ValueError:
-                result = util.json.loads(data[:-2])
-            if (result['ytube']=='0'):
-                vurls = result['file'].split("or")
-                subs = 'sub' in result and result['sub'] or None
-                if subs:
-                    return [{'url':vurls[0].strip(),'quality':'???','subs':subs},
-                                {'url':vurls[1].strip(),'quality':'???','subs':subs}]
-                else:
-                    return [{'url':vurls[0].strip(),'quality':'???'},
-                                {'url':vurls[1].strip(),'quality':'???'}]
-                    
+            headers['Referer'] = BASE_URL + '/flowplayer/flowplayer.commercial-3.2.16.swf'
+            data = util.request(BASE_URL + m2.group(1),headers)
+            m_vurl = re.search("'url':.*?'(http[^']+?mp4)'", data, re.DOTALL)
+            m_surl = re.search("'captionUrl':.*?'(http[^']+)'",data, re.DOTALL)
+            if m_vurl:
+                resolved = {'url':m_vurl.group(1).strip(),'quality':'???'}
+                if m_surl:
+                    resolved['subs'] = m_surl.group(1).strip()
+                return [resolved]
             else:
-                # ignore ytube videos
                 return []
-
 def _regex(url):
     return re.search('video\.anyfiles\.pl/w\.jsp\?id=(?P<id>\d+)',url,re.IGNORECASE | re.DOTALL)
