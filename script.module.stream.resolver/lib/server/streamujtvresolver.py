45296b5e932126eaacde4d417d5382ba8bab57b4
Libor Zoubek <lzoubek@redhat.com>
2014-08-25 21:34:19 +0200

    fix streamujtv resolver

--- script.module.stream.resolver/lib/server/streamujtvresolver.py
+++ script.module.stream.resolver/lib/server/streamujtvresolver.py
@@ -8,7 +8,7 @@
 # */
 import re,util
 import simplejson as json
-from base64 import b64decode
+from base64 import b64decode, b64encode
 
 __name__='streamujtv'
 def supports(url):
@@ -22,6 +22,9 @@ def resolve(url):
         streams = re.search('res0\:[^\"]*\"([^\"]+)',data,re.IGNORECASE|re.DOTALL)
         subs = re.search('sub0\:[^>]+>(http[^\"]+)',data,re.IGNORECASE|re.DOTALL)
         rn = re.search('rn\:[^\"]*\"([^\"]*)',data,re.IGNORECASE|re.DOTALL)
+        if data.find('Toto video neexistuje') > 0:
+            util.error('Video bylo smazano ze serveru')
+            return
         if streams and rn:
             streams = streams.group(1).split(',')
             rn = rn.group(1).split(',')
@@ -30,10 +33,8 @@ def resolve(url):
             headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0',
                     'Referer':'http://www.streamuj.tv/mediaplayer/player.swf'}
             for stream in streams:
-                print stream
-                burl = b64decode('aHR0cDovL2Z1LWNlY2gucmhjbG91ZC5jb20vcGF1dGg/cGxheWVyPWh0dHA6Ly93d3cuc3RyZWFtdWoudHYvbmV3LWZsYXNoLXBsYXllci9tcGx1Z2luMy5zd2YmbGluaz0lcwo=')
-                res = json.loads(util.request(burl % stream))
-                print res
+                burl = b64decode('aHR0cDovL2Z1LWNlY2gucmhjbG91ZC5jb20vcGF1dGg/cGxheWVyPWh0dHA6Ly93d3cuc3RyZWFtdWoudHYvbmV3LWZsYXNoLXBsYXllci9tcGx1Z2luNC5zd2YmbGluaz0lcwo=')
+                res = json.loads(util.request(burl % b64encode(stream)))
                 stream = res['link']
                 q = rn[index]
                 if q == 'HD':
